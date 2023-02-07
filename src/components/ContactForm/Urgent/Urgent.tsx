45f2dc793d55415fd73eddeaef945e43853a4e0d
h8m0ss <danarmenta32@gmail.com>
2023-02-07 15:35:23 -0600

    FEAT: migrated to whatsapp button

--- src/components/ContactForm/Urgent/Urgent.tsx
+++ src/components/ContactForm/Urgent/Urgent.tsx
@@ -1,15 +1,16 @@
+import WhatsappButton from "../../WhatsappButton";
 import style from "./Urgent.module.css";
+
+const urlParams = new URLSearchParams({
+  text: "Buen dia Dra. Dinorah\n",
+});
 
 const Urgent = () => {
   return (
     <div className={style.content}>
-      <p>
-        Si tienes algun problema urgente <br />
-        Envia un mensaje de texto a{" "}
-        <a href="https://wa.me/5551008763">+52 55 5100 8763 </a>
-        <br />
-        Horas de servicio: 9:00AM a 8:00PM (CT)
-      </p>
+      <p>Si tienes algun problema urgente </p>
+      <WhatsappButton href={"https://wa.me/5551008763?" + urlParams} />
+      <p>Horas de servicio: 9:00AM a 8:00PM (CT)</p>
     </div>
   );
 };
